ec4bbf77624e4068f3bebf8157169583c49d1e54
ktk8916 <ktk8916@naver.com>
2022-10-08 20:31:36 +0900

    custom pipe board status check

--- src/board/pipes/board.status.validation.pipe.ts
+++ src/board/pipes/board.status.validation.pipe.ts
@@ -1,13 +1,25 @@
-import { ArgumentMetadata, PipeTransform } from "@nestjs/common";
-import { BoardStatus } from "../board.Model";
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.Model';
 
-export class BoardStatusValidationPipe implements PipeTransform{
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly boardOption = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
 
-    boardOption = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+  transform(value: string, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
 
-    transform(value: any, metadata: ArgumentMetadata) {
-        value = value.toUpperCase();
-        return value;
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is not status`);
     }
 
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    const index = this.boardOption.indexOf(status);
+    return index !== -1;
+  }
 }
